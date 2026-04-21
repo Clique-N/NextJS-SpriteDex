82e8df1e705e1fb9910ae6cb6d0a2aad5ea5d69f
Guilherme <gui.gitgraph@gmail.com>
2026-04-21 17:37:31 -0300

    Terceiro commit

--- src/features/PokemonCard/index.tsx
+++ src/features/PokemonCard/index.tsx
@@ -14,7 +14,7 @@ const PokemonCard = ({poke} : Props) => {
             <Link  className={styles.card__name} href={`/pokemon/${id}`}>
                 <div>
                     <h3>{name}</h3>
-                    <img className={styles.card__img} src={`${process.env.NEXT_PUBLIC_POKE_API_IMG_URL}${id}.png`} alt="" />
+                    <img className={styles.card__img} src={`${process.env.NEXT_PUBLIC_POKE_API_IMG_URL}${poke.id}.png`} alt="" />
                 </div>
             </Link>
         </div>
